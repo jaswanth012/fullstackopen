b234b69dd1114484e0403c230a45d844fc00dddd
jaswanth012 <jaswanthreddy012@gmail.com>
2023-06-18 22:50:01 +0530

    part2

---
+++ part2/courseinfo/src/Course.js
@@ -0,0 +1,38 @@
+const Header = ({ name }) => <h1>{name}</h1>;
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <b>total of {total} exercises</b>;
+};
+
+const Course = ({ courses }) => {
+  return (
+    <div>
+      {courses.map((course) => (
+        <div key={course.id}>
+          <Header key={course.id} name={course.name} />
+          <Content parts={course.parts} />
+          <Total parts={course.parts} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Course;
